d19eddfd10735210cb0aadf94d52c690c9be580c
smallai <hongjie_ai@163.com>
2019-01-28 06:48:05 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,9 +19,17 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user, ImageUploadHandler $uploadHandler)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if ($request->avatar) {
+                $result = $uploadHandler->save($request->avatar, 'avatars', $user->id);
+                if ($request) {
+                    $data['avatar'] = $result['path'];
+                }
+        }
+        $user->update($data);
+
         return redirect()->route('users.show', compact('user'))
             ->with('success', '个人资料更新成功！');
     }
