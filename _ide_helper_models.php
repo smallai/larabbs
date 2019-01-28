152cfc686166269430d449428b6c51828fdbbfbb
smallai <hongjie_ai@163.com>
2019-01-28 08:24:31 +0000

    修复N+1问题

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -44,6 +44,19 @@ namespace App\Models{
 
 namespace App\Models{
 /**
+ * App\Models\Model
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model recent()
+ */
+	class Model extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
  * App\Models\Category
  *
  * @property int $id
@@ -63,5 +76,46 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Category whereUpdatedAt($value)
  */
 	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Topic
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property int $user_id
+ * @property int $category_id
+ * @property int $reply_count
+ * @property int $view_count
+ * @property int $last_reply_user_id
+ * @property int $order
+ * @property string|null $excerpt
+ * @property string|null $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category $category
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model ordered()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Model recent()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereExcerpt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereLastReplyUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereReplyCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Topic whereViewCount($value)
+ */
+	class Topic extends \Eloquent {}
 }
 
