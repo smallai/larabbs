985af568564d5020e6dedb3e9044b530895bb4bc
smallai <hongjie_ai@163.com>
2019-01-28 07:48:19 +0000

    新增 generator 扩展

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.22 on 2019-01-28 14:52:20.
+ * Generated for Laravel 5.7.22 on 2019-01-28 15:48:08.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
