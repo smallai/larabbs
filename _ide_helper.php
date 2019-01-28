b4368572049904afbf34fe4a9d6cc77924745c66
smallai <hongjie_ai@163.com>
2019-01-28 03:47:56 +0000

    生成用户认证代码

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.22 on 2019-01-28 11:20:48.
+ * Generated for Laravel 5.7.22 on 2019-01-28 11:46:59.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
