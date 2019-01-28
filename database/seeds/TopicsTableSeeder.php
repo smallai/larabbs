c7ad40ed3fed73ad4fb4037e0d8608506adb0d2e
smallai <hongjie_ai@163.com>
2019-01-28 08:06:19 +0000

    填充用户和话题

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
 
@@ -7,10 +9,19 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        //所有用户的ID
+        $user_ids = User::all()->pluck('id')->toArray();
+        //所有分类的ID
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        $facker = app(\Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index) use ($user_ids, $category_ids, $facker) {
+                $topic->user_id = $facker->randomElement($user_ids);
+                $topic->category_id = $facker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
